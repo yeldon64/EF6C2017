5d1bf873dd0617340b34be376b4be312d7dd512c
Profe Javi <j.guignard@gmail.com>
2017-11-24 09:28:00 -0300

    EVALUACION!

---
+++ votos/models.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+
+class Distrito(models.Model):
+    """
+    Se decide utilizar este modelo para la clase distrito porque es
+    necesario el nombre y la cantidad de votantes,
+    y en un futuro se mostrara un mapa con un marker por cada distrito
+    que contenga los resultados del mismo.
+    """
+    nombre = models.CharField('Nombre del distrito', max_length=128)
+    cantidad_votantes = models.IntegerField('Cantidad de votantes', default=0)
+    latitude = models.DecimalField('Latitud', max_digits=14, decimal_places=10, default=0)
+    longitude = models.DecimalField('Latitud', max_digits=14, decimal_places=10, default=0)
+
+
+    def __str__(self):
+        return 'Distrito {}'.format(self.nombre)
+
+class Candidato(models.Model):
+    """
+    #TODO Completar segun consideraciones del desarrollador
+    En este comentario escribir por que se decide modelar de esta
+    forma la clase
+    """
+    pass
+
+
+class Votos(models.Model):
+    """
+    #TODO Completar segun consideraciones del desarrollador
+    En este comentario escribir por que se decide modelar de esta
+    forma la clase
+    """
+    pass
---
+++ votos/views.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+from votos.models import *
+
+
+def resultado_global(request):
+    """
+    Generar la vista para devolver el resultado global de la elección.
+    Tener en cuenta que tiene que tener:
+    Cantidad total de votos por candidato
+    Cantidad total de votos nulos
+    Porcentaje de cada candidato
+    Porcentaje de votos nulos
+    Total de votos de la elección
+    """
+    context={}
+    context['distritos'] = Distrito.objects.all()
+    #TODO TU CODIGO AQUI
+
+    return render(request,'global.html',context)
+
+
+def resultado_distrital(request):
+    """
+    Generar la vista para devolver el resultado distrital de la elección
+    Tener en cuenta que tiene que tener:
+    Tamaño del padrón
+    Porcentaje de votos del distrito (respecto al padron. Ejemplo: Si el distrito tiene 1000 votantes y hay 750 votos, el porcentaje es 75%)
+    Total de votos del distrito
+    Candidato ganador
+    """
+    context={}
+
+    #TODO TU CODIGO AQUI
+
+    return render(request,'distrital.html',context)
